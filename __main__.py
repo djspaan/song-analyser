52838e2574b8433fdca410d505b70667975d27dd
djspaan <dennis@spaan.io>
2018-04-08 19:37:23 +0200

    Added wordcloud

--- __main__.py
+++ __main__.py
@@ -3,6 +3,9 @@ from mappers import SongMapper
 from stores import SongStore
 from analyzers import VaderSongAnalyzer
 from analyzers import Rake
+import math
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
 
 SONG_FILE = './data/songdata-all.csv'
 
@@ -21,15 +24,33 @@ class Main:
         Here you can run commands to retrieve the wanted result.
         A few examples are shown below.
         """
-        songs = self.store.all()
 
-        analyzer = Rake(5,2,2)
 
+        # Print the keywords extracted from all songs
+        songs = self.store.all().where('Genre', 'Pop').where('Year', '1992')
+        analyzer = Rake(4, 3, 2)
+        result = []
         for song in songs:
-            print(song.Title + ' - ' + song.Artist)
-            print(analyzer.extract(song.Lyrics))
+            result = result + analyzer.extract(song.Lyrics)
 
+        keywords = []
+        for t in result:
+            for i in range(1,int(math.ceil(t[1]))):
+                keywords.append(t[0])
 
+        wordcloud = WordCloud(background_color='black', scale=3,random_state=1,relative_scaling=0).generate(' '.join(keywords))
+        plt.figure(figsize=(12,10))
+        plt.imshow(wordcloud, interpolation="bilinear")
+        plt.axis("off")
+        plt.show()
+
+        # # Print the keywords extracted from all songs
+        # songs = self.store.all()
+        # analyzer = Rake(4, 3, 2)
+        # for song in songs:
+        #     print('')
+        #     print(song.Title + ' - ' + song.Artist)
+        #     print(analyzer.extract(song.Lyrics))
 
         # # Print all the songs retrieved from the song file.
         # print(self.store.all())
